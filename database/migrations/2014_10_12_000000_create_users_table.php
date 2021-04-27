8352604f61b5adcb1da5190f169e1ddbbb51bcaa
JakyeRU <jakyeru@gmail.com>
2021-04-27 18:17:01 +0300

    Changed $table->bigInteger('id')->primary() to $table->id()

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigInteger('id')->primary();
+			$table->id();
             $table->string('username');
             $table->string('discriminator');
             $table->string('email');
